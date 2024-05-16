19af17ff801f966586ebad6b5e6e53454f347070
Renann77 <renan.dorneles.potencial@gmail.com>
2024-05-16 17:18:12 -0300

    alterações

--- Portifolio/Portfolio/submenu/app.js
+++ Portifolio/Portfolio/submenu/app.js
@@ -1,4 +1,4 @@
-import sublinks from './data.js';
+
 
 const toggleBtn = document.querySelector('.toggle-btn');
 const closeBtn = document.querySelector('.close-btn');
@@ -78,3 +78,31 @@ nav.addEventListener('mouseover', function (e) {
     submenu.classList.remove('show');
   }
 });
+
+const sublinks = [
+  {
+    page: 'products',
+    links: [
+      { label: 'payment', icon: 'fas fa-credit-card', url: 'products.html' },
+      { label: 'terminal', icon: 'fas fa-credit-card', url: 'products.html' },
+      { label: 'connect', icon: 'fas fa-credit-card', url: 'products.html' },
+    ],
+  },
+  {
+    page: 'developers',
+    links: [
+      { label: 'plugins', icon: 'fas fa-book', url: 'products.html' },
+      { label: 'libraries', icon: 'fas fa-book', url: 'products.html' },
+      { label: 'plugins', icon: 'fas fa-book', url: 'products.html' },
+      { label: 'billing', icon: 'fas fa-book', url: 'products.html' },
+    ],
+  },
+  {
+    page: 'company',
+    links: [
+      { label: 'about', icon: 'fas fa-briefcase', url: 'products.html' },
+      { label: 'customers', icon: 'fas fa-briefcase', url: 'products.html' },
+    ],
+  },
+];
+
